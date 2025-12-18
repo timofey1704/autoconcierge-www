1267c224f158ae452b9b55c69c7fb55edb2c7d3f
Timofey <timofey.syr17@gmail.com>
2025-12-18 11:50:55 +0300

    serializers and view logic

---
+++ backend/api/main/serializers/membershipSerializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from sitemanagement.constants.account_types import account_types
+from sitemanagement.models import Feature, Membership
+from django.conf import settings
+
+class FeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feature
+        fields = ['id', 'name']
+        
+class MembershipPlansSerializer(serializers.ModelSerializer):
+    features = FeatureSerializer(many=True, read_only=True)
+    plan = serializers.SerializerMethodField()
+    
+    def get_plan(self, obj):
+        return dict(account_types)[obj.plan]
+    
+    class Meta:
+        model = Membership
+        fields = "__all__"
