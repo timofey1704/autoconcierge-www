e8e70e034cdbadc61b28816def1c1054d56b4122
Timofey <timofey.syr17@gmail.com>
2025-12-22 13:04:16 +0300

    feat / AVT-28 create UCheckbox component

--- frontend/components/FAQ.tsx
+++ frontend/components/FAQ.tsx
@@ -9,26 +9,24 @@ const FAQ: React.FC<FAQProps> = ({ faqs, id }) => {
 
   return (
     <section className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8" id={id}>
-      <div className="py-12 sm:py-16 lg:py-20">
-        <h5 className="gradient-line text-gradient">ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h5>
-        <div className="mt-4 mb-12 flex flex-col gap-6 sm:mb-16 lg:flex-row lg:items-start lg:justify-between lg:gap-8">
-          <h2 className="flex flex-col font-bold text-gray-900">
-            <span>Всё, что вы хотели</span> знать о сервисе
-          </h2>
-          <div className="mt-auto max-w-md text-sm text-gray-600 sm:text-lg lg:text-right">
-            Если не нашли ответ на свой вопрос — всегда можно позвонить на{' '}
-            <a href="tel:+375801100080" className="text-gradient font-semibold transition-colors">
-              8 (801) 100-80-80
-            </a>{' '}
-            и проконсультироваться.
-          </div>
+      <h5 className="gradient-line text-gradient">ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h5>
+      <div className="mt-4 mb-12 flex flex-col gap-6 sm:mb-16 lg:flex-row lg:items-start lg:justify-between lg:gap-8">
+        <h2 className="flex flex-col font-bold text-gray-900">
+          <span>Всё, что вы хотели</span> знать о сервисе
+        </h2>
+        <div className="mt-auto max-w-md text-sm text-gray-600 sm:text-lg lg:text-right">
+          Если не нашли ответ на свой вопрос — всегда можно позвонить на{' '}
+          <a href="tel:+375801100080" className="text-gradient font-semibold transition-colors">
+            8 (801) 100-80-80
+          </a>{' '}
+          и проконсультироваться.
         </div>
+      </div>
 
-        <div className="space-y-3 sm:space-y-4">
-          {faqs.map(faq => (
-            <Accordion key={faq.id} title={faq.title} content={faq.content} />
-          ))}
-        </div>
+      <div className="space-y-3 sm:space-y-4">
+        {faqs.map(faq => (
+          <Accordion key={faq.id} title={faq.title} content={faq.content} />
+        ))}
       </div>
     </section>
   )
