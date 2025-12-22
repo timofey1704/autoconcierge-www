dfdc1e7e56cb8ba98cb528c7c23eb3dd42c9aa13
Timofey <timofey.syr17@gmail.com>
2025-12-22 11:19:05 +0300

    fix / AVT-19 footer fixes

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -5,115 +5,106 @@ import FacebookIcon from '../public/icons/footer/Facebook.svg'
 import LinkedInIcon from '../public/icons/footer/LinkedIn.svg'
 import InstagramIcon from '../public/icons/footer/Instagram.svg'
 import DribbbleIcon from '../public/icons/footer/Dribbble.svg'
-import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full h-[866px] md:h-[636px] xl:h-[407px] bg-black text-white pt-10 px-5 pb-5 lg:pt-15 lg:px-28 lg:pb-10">
-      <div className="flex justify-between flex-wrap gap-10 xl:gap-0 pb-10">
-        <div className="w-[601px]"> 
+    <footer className="h-216.5 w-full bg-black px-5 pt-10 pb-5 text-white md:h-159 lg:px-28 lg:pt-15 lg:pb-10 xl:h-101.75">
+      <div className="flex flex-wrap justify-between gap-10 pb-10 xl:gap-0">
+        <div className="w-150.25">
           <div>
             <div className="flex items-end gap-2">
               <h4>СБЛ</h4>
-              <span className="text-[16px] leading-[24px] lg:text-[22px] lg:leading-[30px] font-normal">Лизинг</span>
+              <span className="text-[16px] leading-6 font-normal lg:text-[22px] lg:leading-7.5">
+                Лизинг
+              </span>
             </div>
-            <p className="pt-[10px]">Круглосуточная помощь на дорогах<br/> по всей Республике Беларусь</p>
+            <p className="pt-2.5">
+              Круглосуточная помощь на дорогах
+              <br /> по всей Республике Беларусь
+            </p>
           </div>
-          <p className="pt-10 lg:pt-9">Сервис предоставляет общество с ограниченной ответственностью
-            «ИнДиЭс Бай», зарегистрированное по адресу Республика
-            Беларусь, 220034, г. Минск, Войсковый пер., д, 12, каб. 4</p>
+          <p className="pt-10 lg:pt-9">
+            Сервис предоставляет общество с ограниченной ответственностью «ИнДиЭс Бай»,
+            зарегистрированное по адресу Республика Беларусь, 220034, г. Минск, Войсковый пер., д,
+            12, каб. 4
+          </p>
         </div>
-        <div className="w-full xl:w-auto flex flex-col justify-between">
-          <div className="flex items-center justify-between flex-wrap gap-5 lg:gap-10">
+        <div className="flex w-full flex-col justify-between xl:w-auto">
+          <div className="flex flex-wrap items-center justify-between gap-5 lg:gap-10">
             <div>
               <div className="flex items-center">
-                <hr className="h-[2px] border-0 gradient-line" />
+                <hr className="gradient-line h-0.5 border-0" />
                 <h5 className="lg:pl-3">ТЕЛЕФОН</h5>
               </div>
-              <div className="flex items-center pt-4">
-                <Image
-                  src={PhoneIcon}
-                  width={44}
-                  height={44}
-                  alt="Телефон"
-                />
-                <h4 className="pl-[10px]">8 (801) 100-80-80</h4>
-              </div>
+              <a href="tel:88011008080">
+                <div className="flex items-center pt-4">
+                  <Image src={PhoneIcon} width={44} height={44} alt="Телефон" />
+                  <h4 className="pl-2.5">8 (801) 100-80-80</h4>
+                </div>
+              </a>
             </div>
             <div>
               <div className="flex items-center">
-                <hr className="h-[2px] border-0 gradient-line" />
+                <hr className="gradient-line h-0.5 border-0" />
                 <h5 className="lg:pl-3">СОЦИАЛЬНЫЕ СЕТИ</h5>
               </div>
               <div className="flex items-center gap-5 pt-4">
-                <a 
+                <a
                   href="https://facebook.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Facebook"
-                  >
-                    <Image 
-                      src={FacebookIcon}
-                      width={44}
-                      height={44}
-                      alt="Facebook"
-                    />
+                >
+                  <Image src={FacebookIcon} width={44} height={44} alt="Facebook" />
                 </a>
-                <a 
+                <a
                   href="https://linkedin.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="LinkedIn"
-                  >
-                    <Image 
-                      src={LinkedInIcon}
-                      width={44}
-                      height={44}
-                      alt="LinkedIn"
-                    />
+                >
+                  <Image src={LinkedInIcon} width={44} height={44} alt="LinkedIn" />
                 </a>
-                <a 
+                <a
                   href="https://instagram.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Instagram"
-                  >
-                    <Image 
-                      src={InstagramIcon}
-                      width={44}
-                      height={44}
-                      alt="Instagram"
-                    />
+                >
+                  <Image src={InstagramIcon} width={44} height={44} alt="Instagram" />
                 </a>
-                <a 
+                <a
                   href="https://Dribbble.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Dribbble"
-                  >
-                    <Image 
-                      src={DribbbleIcon}
-                      width={44}
-                      height={44}
-                      alt="Dribbble"
-                    />
+                >
+                  <Image src={DribbbleIcon} width={44} height={44} alt="Dribbble" />
                 </a>
               </div>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row lg:flex-row justify-start md:justify-between lg:justify-between xl:justify-end flex-wrap gap-5 lg:gap-10 pt-10 xl:pt-24">
-            <Link href="/service-packages" className="internal-link">Пакеты услуг</Link>
-            <Link href="/how-it-works" className="internal-link">Как это работает</Link>
-            <Link href="/partners" className="internal-link">Партнеры</Link>
+          <div className="flex flex-col flex-wrap justify-start gap-5 pt-10 md:flex-row md:justify-between lg:flex-row lg:justify-between lg:gap-10 xl:justify-end xl:pt-24">
+            <Link href="/service-packages" className="internal-link">
+              Пакеты услуг
+            </Link>
+            <Link href="/how-it-works" className="internal-link">
+              Как это работает
+            </Link>
+            <Link href="/partners" className="internal-link">
+              Партнеры
+            </Link>
           </div>
         </div>
       </div>
 
-      <hr className="h-[2px] border-0 bg-(image:--color-gradient) rotate-180" />
+      <hr className="h-0.5 rotate-180 border-0 bg-(image:--color-gradient)" />
 
-      <div className="pt-10 lg:pt-5 flex justify-between flex-wrap gap-[10px]">
+      <div className="flex flex-wrap justify-between gap-2.5 pt-10 lg:pt-5">
         <p>Все права защищены © 2025</p>
-        <Link href="/privacy-policy" className="internal-link">Политика конфиденциальности</Link>
+        <Link href="/privacy-policy" className="internal-link">
+          Политика конфиденциальности
+        </Link>
       </div>
     </footer>
   )
