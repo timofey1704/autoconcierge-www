6d6c719df5caa20682a4c1850920dc17d544c867
Timofey <timofey.syr17@gmail.com>
2025-12-22 11:31:52 +0300

    feat / AVT-17 Pricing tab construction

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,14 @@
 import { getFAQs } from '@/lib/main/fetchFAQ'
 import { getMemberships } from '@/lib/main/fetchMembershipData'
 import FAQ from '@/components/FAQ'
-import UButton from '@/components/ui/UButton'
-import UTextInput from '@/components/ui/UTextInput'
+import PricingTab from '@/components/PricingTab'
 
 export default async function Home() {
   const [faqs, memberships] = await Promise.all([getFAQs(), getMemberships()])
   return (
     <div className="mx-auto flex flex-col items-center justify-center space-y-24 overflow-x-hidden">
-      <UButton text="Кнопка" />
-      <UTextInput
-        value=""
-        name="qwe"
-        placeholder="8 последних цифр"
-        label="Название"
-        helper_text="helper text"
-      />
       <FAQ faqs={faqs} id="faq" />
+      <PricingTab memberships={memberships} id="pricing" />
     </div>
   )
 }
