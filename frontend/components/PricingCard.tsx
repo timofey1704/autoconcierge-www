ae6383f21f52ffa3b43138f9ff550e8a1c7ad063
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2025-12-23 12:00:32 +0300

    Creating base markup

--- frontend/components/PricingCard.tsx
+++ frontend/components/PricingCard.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { PricingCardProps } from '@/app/types'
 import React from 'react'
 
-const PricingCard = () => {
-  return <div>PricingCard</div>
+const PricingCard: React.FC<PricingCardProps> = ({ memberships }) => {
+  return(
+    <div className="w-[289px] h-[610px] flex flex-col items-start rounded-[20px] bg-light-gray px-5 py-15 shadow-lg transition-shadow hover:shadow-xl">
+      {/* <h5 className="text-black">{title}</h5> */}
+        {/* <Image src={icon} width={72} height={72} alt="" /> */}
+        {/* <h5 className="pt-5 pb-[10px] text-black">{title}</h5>
+        <span className="text-4 leading-7 text-dark-gray">{description}</span> */}
+    </div>
+  )
 }
 
 export default PricingCard
--- frontend/components/PricingTab.tsx
+++ frontend/components/PricingTab.tsx
@@ -5,8 +5,42 @@ interface PricingTabProps {
   id: string
 }
 
-const PricingTab: React.FC<PricingTabProps> = ({ memberships, id }) => {
-  return <div>PricingTab</div>
+const PricingTab: React.FC<PricingTabProps> = ({ memberships }) => {
+  return (
+    <div className="max-w-7xl flex flex-col lg:flex-row gap-5 xl:pt-[15px] px-4 sm:px-6 lg:px-8">
+        <div className="w-full lg:max-w-117 xl:max-w-[289px]">
+            <h5 className="gradient-line text-gradient">Тарифы</h5>
+            <div className="w-full flex flex-col pt-[10px]">
+                <h2 className="w-full text-black">Пакеты услуг</h2>
+                <span className="w-full pt-5 text-4 leading-[30px] text-dark-gray">От базовой помощи до максимального комфорта в любой ситуации.</span>
+            </div>
+        </div>
+        <div className="flex gap-5">
+          {/* {memberships.map(item => (
+
+          ))} */}
+        </div>
+        {/* {ServicesData.slice(0, 2).map(item => (
+            <ServiceCard 
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                icon={item.icon}
+            />
+        ))} */}
+    </div>
+
+    // <div className="pricing">
+    //   {memberships.map(item => (
+    //     <div key={item.id}>
+    //       {item.plan} {item.price} {item.features.map(feature => (
+    //         <div key={feature.id}>{feature.name}</div>
+    //       ))}
+    //     </div>
+    //   ))}
+    // </div>
+  )
 }
 
 export default PricingTab
