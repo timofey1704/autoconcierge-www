822f90b48e235c913e43ff6a905182983df58716
Timofey <timofey.syr17@gmail.com>
2026-01-22 14:31:06 +0300

    fix / AVT-169 add actual manager account types

--- manager-frontend/app/constants/accountTypes.ts
+++ manager-frontend/app/constants/accountTypes.ts
@@ -1,25 +1,29 @@
 export const accountTypeToDisplayName = {
-  zooID: 'Зоо ID',
-  concierge: 'Зооконсьерж',
-  zoopolis: 'Зоополис',
+  headOfficeManager: 'Руководитель ГО',
+  headServiceCenterDepartment: 'Начальник отдела ЦУ',
+  specialist: 'Специалист ЦУ',
 } as const
 
 // для бекенда
 export const displayNameToAccountType: Record<string, keyof typeof accountTypeToDisplayName> = {
-  'Зоо ID': 'zooID',
-  Зооконсьерж: 'concierge',
-  Зоополис: 'zoopolis',
+  'Руководитель ГО': 'headOfficeManager',
+  'Начальник отдела ЦУ': 'headServiceCenterDepartment',
+  'Специалист ЦУ': 'specialist',
 }
 
 export const getAccountTypeStyles = (accountType: string) => {
   switch (accountType.toLowerCase()) {
-    case 'zooid':
+    case 'headOfficeManager':
       return 'bg-gray-400 text-white'
-    case 'concierge':
+    case 'headServiceCenterDepartment':
       return 'bg-orange/70 text-white'
-    case 'zoopolis':
+    case 'specialist':
       return 'bg-blue-500 text-white'
     default:
       return 'bg-gray-200'
   }
 }
+
+// 1. Руководитель ГО,
+// 2. Начальник отдела ЦУ
+// 3. Специалист ЦУ
