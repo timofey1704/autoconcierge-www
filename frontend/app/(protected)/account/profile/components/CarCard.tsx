b9bd275874d78621ca68f1d989be492bc314b6a1
Timofey <timofey.syr17@gmail.com>
2026-02-02 11:37:32 +0300

    feat / AVT-67 create car card

--- frontend/app/(protected)/account/profile/components/CarCard.tsx
+++ frontend/app/(protected)/account/profile/components/CarCard.tsx
@@ -1,16 +1,88 @@
 import React from 'react'
+import Image from 'next/image'
 import { Car } from '@/app/types'
+import { getProxiedImageUrl } from '@/lib/utils/imageProxy'
 
 interface CarCardProps {
-  cars: Car[]
+  car: Car
 }
 
-const CarCard: React.FC<CarCardProps> = ({ cars }) => {
+const CarCard: React.FC<CarCardProps> = ({ car }) => {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {cars.map(car => (
-        <div key={car.id}>{car.brand}</div>
-      ))}
+    <div className="rounded-2xl bg-white p-4 shadow-sm transition-all duration-200 hover:shadow-md md:p-6">
+      <h4 className="mb-4">
+        {car.brand} {car.model}
+      </h4>
+      <div className="flex flex-col gap-4 md:flex-row md:gap-6">
+        <div className="flex shrink-0 flex-row items-center justify-around gap-3 md:flex-col md:gap-4">
+          <div className="flex w-32 items-center justify-center md:w-40">
+            <Image
+              src={getProxiedImageUrl(car.image) || '/images/no-car.svg'}
+              alt={`${car.brand} ${car.model}`}
+              height={160}
+              width={160}
+              className="aspect-square rounded-2xl object-cover"
+            />
+          </div>
+
+          <div className="flex flex-col items-center gap-1 md:gap-2">
+            <Image
+              src={getProxiedImageUrl(car.qr_image)}
+              alt="QR Code"
+              width={102}
+              height={102}
+              className="h-24 w-24 rounded-md object-contain md:h-25.5 md:w-25.5"
+            />
+            <div className="flex items-center justify-center rounded-lg bg-gray-100 px-3 py-1.5 md:px-5 md:py-2">
+              <span className="text-[10px] font-semibold text-gray-700 md:text-[12px]">
+                {car.qr_code}
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid flex-1 grid-cols-2 gap-4 md:grid-cols-2">
+          <div>
+            <p className="text-gray-500">Марка</p>
+            <p className="font-semibold text-gray-900">{car.brand}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Модель</p>
+            <p className="font-semibold text-gray-900">{car.model}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Кузов</p>
+            <p className="font-semibold text-gray-900">{car.body_type}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Год выпуска</p>
+            <p className="font-semibold text-gray-900">{car.year_built}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Цвет</p>
+            <p className="font-semibold text-gray-900">{car.color}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">VIN код</p>
+            <p className="font-semibold text-gray-900">{car.vin_code}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Номерной знак</p>
+            <p className="font-semibold text-gray-900">{car.licence_plate}</p>
+          </div>
+
+          <div>
+            <p className="text-gray-500">Лизинговая компания</p>
+            <p className="font-semibold text-gray-900">{car.lising_company}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/app/(protected)/account/profile/forms/ExistedCars.tsx
+++ frontend/app/(protected)/account/profile/forms/ExistedCars.tsx
@@ -10,21 +10,14 @@ interface ExistedCarsProps {
 }
 
 const ExistedCars = ({ onOpenCreateForm, onCarsLoad }: ExistedCarsProps) => {
-  const {
-    data: response,
-    isLoading,
-    error,
-    refetch,
-  } = useClientFetch<{ cars: Car[] }>('/account/cars/')
-
-  const cars = response?.cars || []
+  const { data: cars, isLoading, error, refetch } = useClientFetch<Car[]>('/account/cars/')
 
   // уведомляем родителя о загрузке машин
   useEffect(() => {
-    if (response?.cars) {
-      onCarsLoad(response.cars)
+    if (cars && cars.length > 0) {
+      onCarsLoad(cars)
     }
-  }, [response, onCarsLoad])
+  }, [cars, onCarsLoad])
 
   if (isLoading) {
     return <Loader />
@@ -34,21 +27,27 @@ const ExistedCars = ({ onOpenCreateForm, onCarsLoad }: ExistedCarsProps) => {
     return <div>Ошибка загрузки данных об автомобилях</div>
   }
 
-  if (cars.length === 0) {
-    return (
-      <div className="my-8 space-y-3 rounded-lg bg-white px-2 py-10 text-center">
-        <h2 className="text-2xl!">У вас пока нет добавленных автомобилей</h2>
-        <button
-          onClick={onOpenCreateForm}
-          className="w-full rounded-xl bg-linear-to-r from-blue-700 to-blue-800 px-6 py-3 text-base whitespace-nowrap text-white shadow-lg transition-all duration-200 hover:cursor-pointer hover:from-blue-800 hover:to-blue-900 hover:shadow-xl focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none active:scale-[0.98] md:mx-0 md:w-1/4 md:px-4 md:py-2.5"
-        >
-          Добавить автомобиль
-        </button>
-      </div>
-    )
-  }
-
-  return <CarCard cars={cars} />
+  return (
+    <>
+      {!cars || cars.length === 0 ? (
+        <div className="my-8 space-y-3 rounded-lg bg-white px-2 py-10 text-center">
+          <h2 className="text-2xl!">У вас пока нет добавленных автомобилей</h2>
+          <button
+            onClick={onOpenCreateForm}
+            className="w-full rounded-xl bg-linear-to-r from-blue-700 to-blue-800 px-6 py-3 text-base whitespace-nowrap text-white shadow-lg transition-all duration-200 hover:cursor-pointer hover:from-blue-800 hover:to-blue-900 hover:shadow-xl focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none active:scale-[0.98] md:mx-0 md:w-1/4 md:px-4 md:py-2.5"
+          >
+            Добавить автомобиль
+          </button>
+        </div>
+      ) : (
+        <div className="my-4 space-y-4">
+          {cars?.map(car => (
+            <CarCard key={car.id} car={car} />
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default ExistedCars
